9a64aa5a0082ef9dfb81fc299b48fb231a3e4fa4
AxelTHOMAS29 <85225741+AxelTHOMAS29@users.noreply.github.com>
2023-11-03 15:22:23 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -10,55 +10,59 @@ class Menu {
     getObjDOM() {
         var HTMLUListElement = document.createElement("ul");
         HTMLUListElement.style.display = 'flex';
-        if(`${this.disp}` == 0) {
+        if (`${this.disp}` == 0) {
             HTMLUListElement.style.flexDirection = 'row';
         } else {
             HTMLUListElement.style.flexDirection = 'column';
         }
-          
-        this.label.forEach(element => {
+
+        this.label.forEach((element, index) => {
             var HTMLIElement = document.createElement("li");
             var newContent = document.createTextNode(element);
             HTMLIElement.style.margin = `${this.esp}` + "px";
             HTMLIElement.appendChild(newContent);
             HTMLUListElement.appendChild(HTMLIElement);
-            
+
             HTMLIElement.addEventListener('click', evt => {
-                console.log(`Clic sur ${element}`);
+                const customEvent = new CustomEvent('menu_click', { detail: { index } });
+                HTMLUListElement.dispatchEvent(customEvent);
             });
         });
 
         return HTMLUListElement;
     }
-    setEsp(esp){
+    setEsp(esp) {
         this.esp = esp;
         var HTMLUListElement = document.createElement("ul");
-        this.label.forEach(element => {
+        this.label.forEach((element, index) => {
             var HTMLIElement = document.createElement("li");
             var newContent = document.createTextNode(element);
             HTMLIElement.style.margin = `${this.esp}` + "px";
             HTMLIElement.appendChild(newContent);
             HTMLUListElement.appendChild(HTMLIElement);
-            
+
             HTMLIElement.addEventListener('click', evt => {
-                console.log(`Clic sur ${element}`);
+                const customEvent = new CustomEvent('menu_click', { detail: { index } });
+                HTMLUListElement.dispatchEvent(customEvent);
             });
         }); return HTMLUListElement;
-    } 
-    setDisp(disp){
+    }
+    setDisp(disp) {
         this.disp = disp;
         var HTMLUListElement = document.createElement("ul");
         HTMLUListElement.style.display = 'flex';
-        if(`${this.disp}` == 0) {
+        if (`${this.disp}` == 0) {
             HTMLUListElement.style.flexDirection = 'row';
         } else {
             HTMLUListElement.style.flexDirection = 'column';
         }
     }
-    addItem(nouveau){
+    addItem(nouveau) {
         labels.push(nouveau);
         let menuObj2 = menu.getObjDOM();
         out.replaceChild(menuObj2, menuObj);
+        menuObj2.addEventListener('menu_click', evt =>
+            console.log(`Clic sur ${labels[evt.detail.index]}`));
     }
 }
 
@@ -69,10 +73,13 @@ let menuObj = menu.getObjDOM();
 let out = document.body;
 out.appendChild(menuObj);
 
-function partie2(){
+menuObj.addEventListener('menu_click', evt =>
+    console.log(`Clic sur ${labels[evt.detail.index]}`));
+
+function partie2() {
     menu.setEsp(100);
     menu.setDisp(1);
     menu.addItem('test');
 }
 
-setTimeout(() => partie2(), 5000);
+setTimeout(() => partie2(), 2000);
