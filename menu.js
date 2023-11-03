a573d4c6356982feeb9a5f151ca416e0ba1e66e5
AxelTHOMAS29 <85225741+AxelTHOMAS29@users.noreply.github.com>
2023-11-03 10:47:53 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,61 +1,3 @@
-//document.body.onload = addElement;
-let accueilList = ["bonjour", "test", "test",];
-let produitList = [];
-let contactList = [];
-
-
-function addElement() {
-    // crée un nouvel élément div
-    var newDiv = document.createElement("ul");
-    // et lui donne un peu de contenu
-    var newContent = document.createTextNode("Hi there and greetings!");
-    // ajoute le nœud texte au nouveau div créé
-    newDiv.appendChild(newContent);
-
-    // ajoute le nouvel élément créé et son contenu dans le DOM
-    var currentDiv = document.getElementById("div1");
-    document.body.insertBefore(newDiv, currentDiv);
-}
-
-class Accueil {
-    label;
-    esp;
-    disp;
-    constructor(label = [], esp = 20, disp = 0) {
-        this.label = label;
-        this.esp = esp;
-        this.disp = disp;
-    }
-    createAcceuil() {
-        //console.log(`${this.label}` + " embauche " + `${this.label}` + `${this.esp}` + `${this.disp}`);
-        // crée un nouvel élément div
-        var newUl = document.createElement("ul");
-        this.label.forEach(element => {
-            var newDiv = document.createElement("li");
-            var newContent = document.createTextNode(element);
-            // ajoute le nœud texte au nouveau div créé
-            newDiv.appendChild(newContent);
-
-            // ajoute le nouvel élément créé et son contenu dans le DOM
-            var currentDiv = document.getElementById("div1");
-            document.body.insertBefore(newDiv, currentDiv);
-        });
-    }
-}
-
-function testa() {
-    let newAccueil = new Accueil(accueilList)
-    newAccueil.createAcceuil()
-}
-
-let newAccueil = new Accueil([" bonjour ", " test ", "test"])
-//newAccueil.createAcceuil()
-
-function addArray(v) {
-    accueilList.push(v);
-    //console.log(accueilList)
-}
-
 class Menu {
     label;
     esp;
@@ -66,33 +8,71 @@ class Menu {
         this.disp = disp;
     }
     getObjDOM() {
-        //console.log(`${this.label}` + " embauche " + `${this.label}` + `${this.esp}` + `${this.disp}`);
-        // crée un nouvel élément
-        var currentDiv = document.getElementById("div1");
         var HTMLUListElement = document.createElement("ul");
-        document.body.insertBefore(HTMLUListElement, currentDiv);
-
+        HTMLUListElement.style.display = 'flex';
+        if(`${this.disp}` == 0) {
+            HTMLUListElement.style.flexDirection = 'row';
+        } else {
+            HTMLUListElement.style.flexDirection = 'column';
+        }
+          
         this.label.forEach(element => {
             var HTMLIElement = document.createElement("li");
             var newContent = document.createTextNode(element);
-            // ajoute le texte
+            HTMLIElement.style.margin = `${this.esp}` + "px";
             HTMLIElement.appendChild(newContent);
-
-            // ajoute le nouvel élément créé et son contenu dans le DOM
-            document.body.insertBefore(HTMLIElement, currentDiv);
+            HTMLUListElement.appendChild(HTMLIElement);
             
+            HTMLIElement.addEventListener('click', evt => {
+                console.log(`Clic sur ${element}`);
+            });
         });
+
+        return HTMLUListElement;
+    }
+    setEsp(esp){
+        this.esp = esp;
+        var HTMLUListElement = document.createElement("ul");
+        this.label.forEach(element => {
+            var HTMLIElement = document.createElement("li");
+            var newContent = document.createTextNode(element);
+            HTMLIElement.style.margin = `${this.esp}` + "px";
+            HTMLIElement.appendChild(newContent);
+            HTMLUListElement.appendChild(HTMLIElement);
+            
+            HTMLIElement.addEventListener('click', evt => {
+                console.log(`Clic sur ${labels[evt.detail.index]}`);
+            });
+        }); 
+    } 
+    setDisp(disp){
+        this.disp = disp;
+        var HTMLUListElement = document.createElement("ul");
+        HTMLUListElement.style.display = 'flex';
+        if(`${this.disp}` == 0) {
+            HTMLUListElement.style.flexDirection = 'row';
+        } else {
+            HTMLUListElement.style.flexDirection = 'column';
+        }
+    }
+    addItem(nouveau){
+        labels.push(nouveau);
+        let menuObj2 = menu.getObjDOM();
+        out.replaceChild(menuObj2, menuObj);
     }
 }
 
+
 let labels = ["Accueil", "Produits", "Contact"];
-
 let menu = new Menu(labels);
-
 let menuObj = menu.getObjDOM();
-menuObj.addEventListener("DOMContentLoaded", evt =>
-
-    console.log(`Clic sur ${labels[evt.detail.index]}`));
+let out = document.body;
 out.appendChild(menuObj);
 
-//setTimeout(() => menu.setEsp(100).setDisp(1).addItem('Test'), 5000);
+function partie2(){
+    menu.setEsp(100);
+    menu.setDisp(1);
+    menu.addItem('test');
+}
+
+setTimeout(() => partie2(), 5000);
